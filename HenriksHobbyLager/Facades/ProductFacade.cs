94485883602066b0de7104d1fb6fa960f18c9a59
dsilentghost <94parjaz@gafe.molndal.se>
2024-11-25 11:29:18 +0100

    Refactor: Created productfacade.

---
+++ HenriksHobbyLager/Facades/ProductFacade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HenriksHobbyLager.Interfaces;
+using RefactoringExercise;
+
+namespace HenriksHobbyLager.Facades
+{
+    internal class ProductFacade : IProductFacade
+    {
+        public void CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> SearchProducts(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
