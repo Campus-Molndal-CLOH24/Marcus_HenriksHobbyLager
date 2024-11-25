81fefa14a074f4c634e80431bd2bc12a3354baaa
Antonhejhejhej <123376072+Antonhejhejhej@users.noreply.github.com>
2024-11-25 09:37:44 +0100

    Refactor: Moved IRepository to separate file in 'interfaces' folder.

---
+++ HenriksHobbyLager/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactoringExercise.Interfaces;
+
+internal interface IRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(int id);
+    IEnumerable<T> Search(Func<T, bool> predicate);
+}
